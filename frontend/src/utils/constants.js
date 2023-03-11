240b9bef9467959e3b381eda61f254fbf675ef5a
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2023-03-11 19:49:14 +0200

    fix: https

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = 'http://api.kolosov.kypipodariday.nomoredomains.work';
+export const URL = 'https://api.kolosov.kypipodariday.nomoredomains.work';
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
